c9ae70553fdb535ae8dd2a02747c29e43f5f7fb2
RKB <balaji648balaji@gmail.com>
2024-12-20 15:11:49 +0530

    added pages

---
+++ src/pages/home.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../homecomps/home.css";
+import Logo from "../assets/logo.jpeg";
+
+const Home = () => {
+  return (
+
+    <div>
+      <img src="Logo" alt="logo" />
+      <h1 id='sname'>SQUAD 76</h1>
+      <h2 id='qs'>"Powered by ambition, defined by success."</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/inovation_atlas">Inovation Atlas</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/memory_valut">Memory Vault</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Home;
